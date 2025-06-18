6ad5b778d44a0cff7f706daaf02e0c83dd68532a
Josieli_Ramos <josielisouzaderamos714@gmail.com>
2025-06-18 18:58:01 -0300

    Adicionado carrossel infinito na home

--- scripts/animacoes.js
+++ scripts/animacoes.js
@@ -54,4 +54,12 @@ export function closeAllAccordions() {
 }
 
 
+let carousel = document.querySelector( '.carousel' ),
+    track = carousel.querySelector( '.carousel--track' ),
+    row = carousel.querySelector( '.carousel--row' );
+
+let clonedRow = row.cloneNode( true );
+
+track.appendChild( clonedRow );
+
 
